65206eb524acf4cf67714f464d18e7a98e513ab0
Muhammad Hamid <61172566+hamidkhans651@users.noreply.github.com>
2025-06-29 19:09:53 -0700

    update-for-vercel

--- backend/api/server.js
+++ backend/api/server.js
@@ -2,8 +2,8 @@ const path = require('path');
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
-const { errorHandler } = require('./middleware/errorMiddleware');
-const connectDB = require('./config/db');
+const { errorHandler } = require('../middleware/errorMiddleware');
+const connectDB = require('../config/db');
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -13,8 +13,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/tasks', require('./routes/taskRoutes'));
-app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/tasks', require('../routes/taskRoutes'));
+app.use('/api/users', require('../routes/userRoutes'));
 
 // Serve frontend
 if (process.env.NODE_ENV === 'production') {
